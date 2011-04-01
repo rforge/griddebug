1cdc7c9dc844475c30eb3b2ca9130e3089a38a54
paul <paul@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-01 00:28:01 +0000

    initial check in of gridDebug package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/griddebug@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/svg.R
@@ -0,0 +1,58 @@
+
+# General function to apply a function to each element of the
+# grid display list - should end up in 'grid' itself
+grid.DLapply <- function(fun, ...) {
+    # Traverse DL and do something to each entry
+    gridDL <- grid:::grid.Call.graphics("L_getDisplayList")
+    gridDLindex <- grid:::grid.Call.graphics("L_getDLindex")
+    for (i in 1:gridDLindex) {
+        elt <- grid:::grid.Call.graphics("L_getDLelt", i)
+        grid:::grid.Call.graphics("L_setDLindex", i)
+        grid:::grid.Call.graphics("L_setDLelt", fun(elt, ...))
+    }
+    grid:::grid.Call.graphics("L_setDLindex", gridDLindex)
+}
+
+# Add tooltip attributes to a graph node on the DL
+garnishNodes <- function(elt) {
+    if (inherits(elt, "gTree")) {
+        garnishGrob(elt,
+                    onmouseover=paste("showTooltip(evt, '",
+                      gsub("\n", "", elt$children[[2]]$label), "')",
+                      sep=""),
+                    onmouseout="hideTooltip()")
+    } else {
+        elt
+    }
+}
+
+curveToDev <- function(curve, col, lwd, lty, dev) {
+    primToDev(curveGrob(curve, col, lwd, lty), dev)
+}
+
+# primToDev method for "edgegrob" grobs
+primToDev.edgegrob <- function(x, dev) {
+    edge <- x$edge
+    # FIXME:  assumes arrow at end of edge
+    splines <- splines(edge)
+    n <- length(splines)
+    col <- color(edge)
+    lapply(splines, curveToDev, col=col, lwd=edge@lwd, lty=edge@lty, dev=dev)
+    lastCP <- pointList(splines[[n]])[[4]]
+    arrow <- arrow(angle=10, type="closed", length=x$arrowlen)
+    primToDev(segmentsGrob(lastCP[1], lastCP[2],
+                           getX(ep(edge)), getY(ep(edge)),
+                           default.units="native",
+                           arrow=arrow,
+                           gp=gpar(col=col, fill=col, lwd=edge@lwd,
+                             lty=edge@lty)),
+              dev)
+}
+
+# User interface
+addTooltips <- function(filename="Rplots.svg") {
+    require(gridSVG)
+    grid.DLapply(garnishNodes)
+    grid.script(filename=system.file("js", "tooltip.js", package="gridDebug"))
+    gridToSVG(filename)
+}
