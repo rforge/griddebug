6c7abc10095381feabf9b7b2ebb9adf7c702d47f
paul <paul@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-01 00:40:34 +0000

    changes that depend on R 2.16; removal of special primToDev() methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/griddebug@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/R2.16/pkg/R/svg.R
+++ branches/R2.16/pkg/R/svg.R
@@ -16,37 +16,6 @@ garnishNodes <- function(elt) {
     }
 }
 
-primToDev.beziergrob <- function(x, dev) {
-    primToDev(grid:::splinegrob(x), dev)
-}
-
-curveToDev <- function(curve, col, lwd, lty, dev) {
-    primToDev(gridGraphviz:::curveGrob(curve, col, lwd, lty), dev)
-}
-
-# primToDev method for "edgegrob" grobs
-primToDev.edgegrob <- function(x, dev) {
-    edge <- x$edge
-    if (!length(edge@lwd))
-        edge@lwd <- 1    
-    if (!length(edge@lty))
-        edge@lty <- "solid"
-    # FIXME:  assumes arrow at end of edge
-    splines <- splines(edge)
-    n <- length(splines)
-    col <- color(edge)
-    lapply(splines, curveToDev, col=col, lwd=edge@lwd, lty=edge@lty, dev=dev)
-    lastCP <- pointList(splines[[n]])[[4]]
-    arrow <- arrow(angle=10, type="closed", length=x$arrowlen)
-    primToDev(segmentsGrob(lastCP[1], lastCP[2],
-                           getX(ep(edge)), getY(ep(edge)),
-                           default.units="native",
-                           arrow=arrow,
-                           gp=gpar(col=col, fill=col, lwd=edge@lwd,
-                             lty=edge@lty)),
-              dev)
-}
-
 # User interface
 gridTreeTips <- function(filename="Rplots.svg", ..., grid=TRUE) {
     if (!grid)
